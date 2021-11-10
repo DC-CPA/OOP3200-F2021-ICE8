6a515bae9ae03623b1832ec00187acd2e250fe5e
Katherine Bellman <katherine.bellman@dcmail.ca>
2021-11-09 20:53:47 -0500

    initial commit

---
+++ src/ca/durhamcollege/Main.java
@@ -0,0 +1,16 @@
+/** Main.java
+ * @author Katherine Bellman , studentID: 100325825
+ * @version 0.0.1
+ *
+ * @date November 9th 2021
+ *
+ * @Note: ICE 8 Java logic, arrays and strings
+ */
+package ca.durhamcollege;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
